85d676249ff7285ed2f88210cbc41de11f653cb4
Subhajit Sahu <wolfram77@gmail.com>
2021-10-21 18:11:53 +0530

    :skunk: update src

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-src="pagerank-adjust-tolerance-function"
+src="pagerank-levelwise-adjust-tolerance-function"
 out="/home/resources/Documents/subhajit/$src.log"
 ulimit -s unlimited
 printf "" > "$out"
--- src/pagerank.hxx
+++ src/pagerank.hxx
@@ -8,18 +8,25 @@ using std::move;
 
 
 
+// PAGERANK-OPTIONS
+// ----------------
+
 template <class T>
 struct PagerankOptions {
   int repeat;
-  int toleranceNorm;
   T   damping;
   T   tolerance;
   int maxIterations;
 
-  PagerankOptions(int repeat=1, int toleranceNorm=1, T damping=0.85, T tolerance=1e-6, int maxIterations=500) :
-  repeat(repeat), toleranceNorm(toleranceNorm), damping(damping), tolerance(tolerance), maxIterations(maxIterations) {}
+  PagerankOptions(int repeat=1, T damping=0.85, T tolerance=1e-6, int maxIterations=500) :
+  repeat(repeat), damping(damping), tolerance(tolerance), maxIterations(maxIterations) {}
 };
 
+
+
+
+// PAGERANK-RESULT
+// ---------------
 
 template <class T>
 struct PagerankResult {

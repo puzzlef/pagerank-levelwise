cc77b5fef2cbfe6243422ee42999271c8b3cd1fc
Subhajit Sahu <wolfram77@gmail.com>
2021-12-11 03:22:10 +0530

    :turtle: w/ expt

--- main.cxx
+++ main.cxx
@@ -15,39 +15,34 @@ void runPagerank(const G& x, const H& xt, int repeat) {
   enum NormFunction { L0=0, L1=1, L2=2, Li=3 };
   vector<float> *init = nullptr;
 
-  // Find pagerank using default options.
-  auto a0 = pagerankMonolithicSeq(xt, init, {repeat});
-  auto e0 = l1Norm(a0.ranks, a0.ranks);
-  printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankMonolithicSeq\n", a0.time, a0.iterations, e0);
-
   // Find pagerank using L1-norm for convergence check.
   auto a1 = pagerankMonolithicSeq(xt, init, {repeat, L1});
-  auto e1 = l1Norm(a1.ranks, a0.ranks);
+  auto e1 = l1Norm(a1.ranks, a1.ranks);
   printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankMonolithicSeqL1Norm\n", a1.time, a1.iterations, e1);
-  for (int CM=1, i=0; CM<x.order(); CM*=i&1? 2:5, i++) {
-    auto a2 = pagerankLevelwiseSeq(x, xt, init, {repeat, L1, CM});
-    auto e2 = l1Norm(a2.ranks, a0.ranks);
-    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankLevelwiseSeqL1Norm [min-compute-size=%.0e]\n", a2.time, a2.iterations, e2, (double) CM);
+  for (int MC=1, i=0; MC<=5e+7; MC*=i&1? 2:5, i++) {
+    auto a2 = pagerankLevelwiseSeq(x, xt, init, {repeat, L1, MC});
+    auto e2 = l1Norm(a2.ranks, a1.ranks);
+    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankLevelwiseSeqL1Norm [min-compute=%d]\n", a2.time, a2.iterations, e2, MC);
   }
 
   // Find pagerank using L2-norm for convergence check.
   auto a3 = pagerankMonolithicSeq(xt, init, {repeat, L2});
-  auto e3 = l1Norm(a3.ranks, a0.ranks);
+  auto e3 = l1Norm(a3.ranks, a1.ranks);
   printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankMonolithicSeqL2Norm\n", a3.time, a3.iterations, e3);
-  for (int CM=1, i=0; CM<x.order(); CM*=i&1? 2:5, i++) {
-    auto a4 = pagerankLevelwiseSeq(x, xt, init, {repeat, L2, CM});
-    auto e4 = l1Norm(a4.ranks, a0.ranks);
-    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankLevelwiseSeqL2Norm [min-compute-size=%.0e]\n", a4.time, a4.iterations, e4, (double) CM);
+  for (int MC=1, i=0; MC<=5e+7; MC*=i&1? 2:5, i++) {
+    auto a4 = pagerankLevelwiseSeq(x, xt, init, {repeat, L2, MC});
+    auto e4 = l1Norm(a4.ranks, a1.ranks);
+    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankLevelwiseSeqL2Norm [min-compute=%d]\n", a4.time, a4.iterations, e4, MC);
   }
 
   // Find pagerank using Li-norm for convergence check.
   auto a5 = pagerankMonolithicSeq(xt, init, {repeat, Li});
-  auto e5 = l1Norm(a5.ranks, a0.ranks);
+  auto e5 = l1Norm(a5.ranks, a1.ranks);
   printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankMonolithicSeqLiNorm\n", a5.time, a5.iterations, e5);
-  for (int CM=1, i=0; CM<x.order(); CM*=i&1? 2:5, i++) {
-    auto a6 = pagerankLevelwiseSeq(x, xt, init, {repeat, Li, CM});
-    auto e6 = l1Norm(a6.ranks, a0.ranks);
-    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankLevelwiseSeqLiNorm [min-compute-size=%.0e]\n", a6.time, a6.iterations, e6, (double) CM);
+  for (int MC=1, i=0; MC<=5e+7; MC*=i&1? 2:5, i++) {
+    auto a6 = pagerankLevelwiseSeq(x, xt, init, {repeat, Li, MC});
+    auto e6 = l1Norm(a6.ranks, a1.ranks);
+    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankLevelwiseSeqLiNorm [min-compute=%d]\n", a6.time, a6.iterations, e6, MC);
   }
 }
 
